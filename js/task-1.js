77b3d20eaed7a8c8479115075825d6b5af521c13
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2023-12-03 00:26:21 +0200

    add 3 taskes

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function makeTransaction(quantity, pricePerDroid) {
+  let totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+makeTransaction(5, 300);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
